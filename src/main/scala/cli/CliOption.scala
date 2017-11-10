f1cf50349ef4756d082077449a10a98d1ff79c20
Jia Chen <grievejia@gmail.com>
2017-11-10 14:59:09 -0600

    Made RunConfig configurable from the cli. Note that options that are not commonly used are all hidden from the usage texts.

--- src/main/scala/cli/CliOption.scala
+++ src/main/scala/cli/CliOption.scala
@@ -4,6 +4,14 @@ case class CliOption(target: String = "",
                      ioId: Int = 0,
                      seeds: Seq[Int] = Seq(0),
                      disableGui: Boolean = false,
+                     populationSize: Int = 500,
+                     tournamentSize: Int = 7,
+                     evaluationTrials: Int = 3,
+                     totalSizeTolerance: Int = 50,
+                     singleSizeTolerance: Int = 30,
+                     threadNum: Int = 1,
+                     timeLimitInMillis: Int = 10000,
+                     maxNonIncreaseTime: Int = 150,
                      extrapolatePattern: Option[ExtrapolationArgs] = None
                     )
 
