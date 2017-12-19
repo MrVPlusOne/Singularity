58106c5a2676d2934495cb3f46805c9e36d77071
Jia Chen <grievejia@gmail.com>
2017-12-19 15:15:59 -0600

    Fix a bug where maxNonIncreaseGen cannot be set through the command line

--- src/main/scala/cli/BenchmarkDriver.scala
+++ src/main/scala/cli/BenchmarkDriver.scala
@@ -77,7 +77,7 @@ object BenchmarkDriver {
   def getRunConfig(option: CliOption, size: Int): RunConfig = {
     import option._
 
-    val execConfigOverride = execConfig.copy(evalSizePolicy = FixedEvalSize(size), maxNonIncreaseGen = None)
+    val execConfigOverride = execConfig.copy(evalSizePolicy = FixedEvalSize(size))
 
     RunConfig(runnerConfig, gpConfig, execConfigOverride)
   }
