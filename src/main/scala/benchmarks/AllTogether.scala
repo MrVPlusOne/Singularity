699100aa85d5fa8fd19f5efb43a5f1d42c79b1ce
Jiayi Wei <wjydzh1@gmail.com>
2018-02-11 10:41:07 -0600

    Add SlowFuzz Regex examples and STAC examples for AllTogether

--- src/main/scala/benchmarks/AllTogether.scala
+++ src/main/scala/benchmarks/AllTogether.scala
@@ -7,25 +7,33 @@ import patsyn._
 import scala.util.Random
 
 object AllTogether {
-  type ConfigGen = String => ProblemConfig
+  type ConfigGen = (Int, String) => ProblemConfig
 
-  def toGen(pc: ProblemConfig): ConfigGen = _ => pc
+  def toGen(pc: ProblemConfig): ConfigGen = (_,_) => pc
 
-  def allProblems: IS[ConfigGen] = VavrExamples.all.map(toGen) ++ SlowfuzzExamples.allExceptRegex ++
+  def toGen(fPc: String => ProblemConfig): ConfigGen = (_, dir) => fPc(dir)
+
+  def allProblems: IS[ConfigGen] = VavrExamples.all.map(toGen) ++ SlowfuzzExamples.allExceptRegex.map(toGen) ++
     IS(CommonsExamples.curveFittingProblem("polyFit5", PolynomialCurveFitter.create(5)),
       CommonsExamples.curveFittingProblem("harmonicFit", HarmonicCurveFitter.create()),
       CommonsExamples.curveFittingProblem("gaussianFit", GaussianCurveFitter.create())
     ).map(toGen) ++
-    IS(DemanglerExamples.llvmDemanglerExample, DemanglerExamples.lldbDemanglerExample) ++
+    IS(DemanglerExamples.llvmDemanglerExample, DemanglerExamples.lldbDemanglerExample).map(toGen) ++
     GuavaExamples.all.map(toGen) ++
     (JGraphTExamples.maxFlowProblems ++ JGraphTExamples.spanningTreeProblems.map(_._1) ++
       JGraphTExamples.coloringProblems ++ JGraphTExamples.simpleCyclesProblems).map(toGen)
 
+  /** Regexes from SlowFuzz */
+  def allProblems2: IS[ConfigGen] = SlowfuzzExamples.allRegexProblems.map(toGen)
+
+
   def main(args: Array[String]): Unit = {
-    val problems = allProblems
+    val problems = allProblems2
     val epochNum = 20
     val taskNum = problems.length * epochNum
     val fuzzingTimeLimit = 2*3600
+
+    val seedBase = allProblems.length * epochNum
 
     SimpleMath.processMap(args, 0 until taskNum,
       processNum = 35,
@@ -33,8 +41,8 @@ object AllTogether {
     ){
       i =>
         val problemId = i % problems.length
-        val ioId = i
-        val p = problems(problemId)(FileInteraction.getWorkingDir(ioId = ioId))
+        val ioId = i + seedBase
+        val p = problems(problemId)(ioId, FileInteraction.getWorkingDir(ioId = ioId))
         val rand = new Random(ioId)
         Supernova.standardSupernova.fuzzProblem(p,
           RunnerConfig(ioId = ioId, randomSeed= ioId, useGUI = false, callExitAfterFinish = false),
